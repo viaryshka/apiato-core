581cac38da60f37782e3508372c3bb4a6908a6b6
Anton Viaryshka <anton.verishko@gmail.com>
2025-09-01 16:29:18 +0300

    Adds meta data to API controller responses.

--- src/Abstracts/Controllers/ApiController.php
+++ src/Abstracts/Controllers/ApiController.php
@@ -6,16 +6,23 @@ use Illuminate\Http\JsonResponse;
 
 abstract class ApiController extends Controller
 {
-    public function responseWithTransform(mixed $data, string $transformerClass, int $status = 200): JsonResponse
-    {
+    public function responseWithTransform(
+        mixed $data,
+        string $transformerClass,
+        int $status = 200,
+        array $meta = []
+    ): JsonResponse {
         $data = fractal($data, new $transformerClass)->toArray();
+        if (! empty($meta)) {
+            $data['meta'] = $meta;
+        }
 
         return new JsonResponse($data, $status);
     }
 
-    public function responseWithCreatedTransform(mixed $data, string $transformerClass): JsonResponse
+    public function responseWithCreatedTransform(mixed $data, string $transformerClass, array $meta = []): JsonResponse
     {
-        return $this->responseWithTransform($data, $transformerClass, 201);
+        return $this->responseWithTransform($data, $transformerClass, 201, $meta);
     }
 
     public function json($data, $status = 200, array $headers = [], $options = 0): JsonResponse
