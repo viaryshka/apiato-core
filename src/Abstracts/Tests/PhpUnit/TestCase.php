84d0a9d40c8ca87c80847161c0e9dfde0ca6c918
Anton Viaryshka <anton.verishko@gmail.com>
2025-09-17 13:24:17 +0300

    Adds Laravel's `LazilyRefreshDatabase` trait to TestCase.

--- src/Abstracts/Tests/PhpUnit/TestCase.php
+++ src/Abstracts/Tests/PhpUnit/TestCase.php
@@ -2,6 +2,10 @@
 
 namespace Apiato\Core\Abstracts\Tests\PhpUnit;
 
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends BaseTestCase
+{
+    use LazilyRefreshDatabase;
+}
